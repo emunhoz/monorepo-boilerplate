917f9b38e18324577882eee953e9ce1580148470
emunhoz <edermunhozsantos@gmail.com>
2021-02-23 02:43:15 -0300

    test: check button from monorepo exist

--- packages/cra/src/App.test.tsx
+++ packages/cra/src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { render, screen } from '../test-utils'
+import { render, screen } from './utils/test-utils'
 import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
+})
+
+test('renders button from @monorepo/ui-components', () => {
+  render(<App />)
+  const buttonMonorepo = screen.getByRole('button', { name: /button from @monorepo\/ui\-components/i })
+  expect(buttonMonorepo).toBeInTheDocument()
 })
