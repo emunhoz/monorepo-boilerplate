e7b024502b6fffa82fc5289af88ac5387e3164bc
emunhoz <edermunhozsantos@gmail.com>
2022-02-14 17:33:36 -0300

    chore(deps): fix storybook ci

--- packages/ui-components/.storybook/main.js
+++ packages/ui-components/.storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
+  core: {
+    builder: "webpack5",
+  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
@@ -6,8 +9,7 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-postcss"
+    "@storybook/addon-interactions"
   ],
   "framework": "@storybook/react"
 }
