d42909c16bb443db2ae1330bd25a660e1273e2bc
emunhoz <edermunhozsantos@gmail.com>
2022-02-14 10:19:40 -0300

    refactor: update ui-components dependencies

--- packages/ui-components/.eslintrc.js
+++ packages/ui-components/.eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     "es2021": true,
     "node": true
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended", "plugin:storybook/recommended"],
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended", "plugin:storybook/recommended", "plugin:storybook/recommended"],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaFeatures": {
--- packages/ui-components/.storybook/main.js
+++ packages/ui-components/.storybook/main.js
@@ -5,7 +5,9 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-postcss"
   ],
   "framework": "@storybook/react"
 }
