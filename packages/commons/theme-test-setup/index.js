543337c041f77c53447dff0345d348f9badae93f
emunhoz <edermunhozsantos@gmail.com>
2022-04-11 23:21:14 -0300

    chore(ci): change test coverage action

--- packages/commons/theme-test-setup/index.js
+++ packages/commons/theme-test-setup/index.js
@@ -1,17 +1,16 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ThemeProvider } from '@emotion/react'
 import createTheme from '@monorepo/design-tokens'
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import React from 'react'
 
-const AllTheProviders = ({ children }: any) => (
+const AllTheProviders = ({ children }) => (
   <ThemeProvider theme={createTheme}>
     {children}
   </ThemeProvider>
 )
 
-const customRender: any = (ui: any, options?: any) => render(ui, { wrapper: AllTheProviders, ...options })
+const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options })
 
 // re-export everything
 export * from '@testing-library/react'
